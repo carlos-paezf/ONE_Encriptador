6bc4e7f6b3bead24f9f4d18852b35eb51b3867da
Carlos David Paez Ferreira <cpaezferreira@gmail.com>
2023-04-13 09:29:37 -0500

    Add Documentation

--- script.js
+++ script.js
@@ -77,11 +77,21 @@ const decryptText = (text) => {
 };
 
 
+/* This code adds an event listener to the `formElement` that listens for a `submit` event. When the
+form is submitted, the default behavior of the browser is prevented by calling the
+`preventDefault()` method on the event object `e`. This prevents the page from reloading or
+navigating to a new page when the form is submitted. */
 formElement.addEventListener('submit', (e) => {
     e.preventDefault();
 });
 
 
+/* This code adds an event listener to the `inputElement` that listens for any changes to its value.
+When the input value changes, the function inside the event listener is executed. It gets the
+current value of the input field and passes it to the `normalizeText()` function, which normalizes
+and removes special characters from the text. The normalized text is then set as the new value of
+the input field. This ensures that the input text is always normalized and ready for encryption or
+decryption. */
 inputElement.addEventListener('input', (e) => {
     const text = e.target.value;
 
@@ -89,6 +99,11 @@ inputElement.addEventListener('input', (e) => {
 });
 
 
+/* This code adds an event listener to the `btnEncrypter` button, which listens for a click event. When
+the button is clicked, it calls the `encryptText()` function with the value of the `inputElement` as
+its argument. It then adds the `hide` class to the `bannerElement` and removes the `hide` class from
+the `withResultElement`. Finally, it sets the `innerHTML` of the `resultTextElement` to the
+encrypted text returned by the `encryptText()` function. */
 btnEncrypter.addEventListener('click', () => {
     const text = encryptText(inputElement.value);
 
@@ -99,6 +114,11 @@ btnEncrypter.addEventListener('click', () => {
 });
 
 
+/* This code adds an event listener to the `btnDecrypt` button, which listens for a click event. When
+the button is clicked, it calls the `decryptText()` function with the value of the `inputElement` as
+its argument. It then adds the `hide` class to the `bannerElement` and removes the `hide` class from
+the `withResultElement`. Finally, it sets the `innerHTML` of the `resultTextElement` to the
+decrypted text returned by the `decryptText()` function. */
 btnDecrypt.addEventListener('click', () => {
     const text = decryptText(inputElement.value);
 
@@ -109,6 +129,11 @@ btnDecrypt.addEventListener('click', () => {
 });
 
 
+/* This code adds an event listener to the `btnCopy` button, which listens for a click event. When the
+button is clicked, it copies the text content of the `resultTextElement` to the clipboard using the
+`navigator.clipboard.writeText()` method. If the text is successfully copied, it displays an alert
+message saying "Texto copiado en el clipboard". If there is an error while copying, it logs an error
+message to the console. */
 btnCopy.addEventListener('click', () => {
     navigator.clipboard.writeText(resultTextElement.innerText)
         .then(() => alert('Texto copiado en el clipboard'))
